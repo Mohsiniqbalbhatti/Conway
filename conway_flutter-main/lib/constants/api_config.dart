211f10745a9a843b352a512c1dca70f936c77c54
Mohsin Iqbal <mohsiniqbalbhatti0024@gmail.com>
2025-04-28 02:00:40 +0500

    updated favicons and api url

--- conway_flutter-main/lib/constants/api_config.dart
+++ conway_flutter-main/lib/constants/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Change this to your server's IP address
-  static const String baseUrl = 'http://192.168.1.14:3000/api';
+  static const String baseUrl = 'http://56.228.5.124:3000/api';
 
   // Authentication endpoints
   static const String requestOtp = '$baseUrl/request-otp';
